e627057390f719be93eb8f3f34c25e2f4b41e213
lcjefferson <77124891+lcjefferson@users.noreply.github.com>
2026-01-17 19:54:58 -0300

    fix: use wildcard origin for CORS debugging

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: '*',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
